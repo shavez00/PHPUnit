3e08a448a2269713ce3c0d4e1b90226777bcdacd
Shavez <shavez00@Yahoo.com>
2023-01-14 23:21:28 +0000

    updated octagon class and tests

--- Octagon.php
+++ Octagon.php
@@ -1,8 +1,16 @@
 <?php
 
 class Octagon {
+    private $sideLength = null;
     public function __construct($sideLength) {
+        $this->sideLength = $sideLength;
+    }
 
-        
+    public function getPerimeter() {
+        return $this->sideLength*8;
+    }
+
+    public function getArea() {
+        return round((2*pow($this->sideLength,2))*(1+sqrt(2)),6);
     }
 }
--- test/OctagonCalculatorTest.php
+++ test/OctagonCalculatorTest.php
@@ -4,9 +4,27 @@ use PHPUnit\Framework\TestCase;
 require __DIR__ . "/../Octagon.php";
 
 class OctagonCalculatorTest extends TestCase {
-    function testInitiateClass()
+    public function testInitiateClass()
     {
         $octagonClass = new Octagon(5);
+
+        $this->assertTrue(true);
+        $this->assertInstanceOf("Octagon", $octagonClass);
+    }
+
+    public function testCalculatePerimeterWithSideLength10() {
+        $octagonCalculator = new Octagon(10);
+        $perimeter = $octagonCalculator->getPerimeter();
+
+        $this->assertEquals(80, $perimeter);
+    }
+
+    public function testCalculateAreaOfAnOctagonWithSideLength10() {
+        $octagonAreaCalculator = new Octagon(10);
+        $area = $octagonAreaCalculator->getArea();
+
+        $this->assertEquals(482.842712, $area);
+
     }
 
 }
