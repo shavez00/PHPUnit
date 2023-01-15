8ad5d3620938d16fb2ff7b4f4257bb852816dcab
Shavez <shavez00@Yahoo.com>
2023-01-15 21:35:56 +0000

    updated tests to make it easier

--- test/OctagonCalculatorTest.php
+++ test/OctagonCalculatorTest.php
@@ -4,26 +4,28 @@ use PHPUnit\Framework\TestCase;
 require __DIR__ . "/../Octagon.php";
 
 class OctagonCalculatorTest extends TestCase {
+    public $testParameter = ["side"=>10,"perimeter"=>80,"area"=>482.842712];
+
     public function testInitiateClass()
     {
-        $octagonClass = new Octagon(5);
+        $octagonClass = new Octagon($this->testParameter["side"]);
 
         $this->assertTrue(true);
         $this->assertInstanceOf("Octagon", $octagonClass);
     }
 
     public function testCalculatePerimeterWithSideLength10() {
-        $octagonCalculator = new Octagon(10);
+        $octagonCalculator = new Octagon($this->testParameter["side"]);
         $perimeter = $octagonCalculator->getPerimeter();
 
-        $this->assertEquals(80, $perimeter);
+        $this->assertEquals($this->testParameter["perimeter"], $perimeter);
     }
 
     public function testCalculateAreaOfAnOctagonWithSideLength10() {
-        $octagonAreaCalculator = new Octagon(10);
+        $octagonAreaCalculator = new Octagon($this->testParameter["side"]);
         $area = $octagonAreaCalculator->getArea();
 
-        $this->assertEquals(482.842712, $area);
+        $this->assertEquals($this->testParameter["area"], $area);
 
     }
 
