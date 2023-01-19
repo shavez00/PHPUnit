d355ba7fa872e6283796338cbf0c852d318017f2
Shavez <shavez00@Yahoo.com>
2023-01-19 01:30:35 +0000

    updated Octagon test with setup and teardown

--- test/OctagonCalculatorTest.php
+++ test/OctagonCalculatorTest.php
@@ -4,26 +4,30 @@ use PHPUnit\Framework\TestCase;
 require __DIR__ . "/../src/Octagon.php";
 
 class OctagonCalculatorTest extends TestCase {
-    private $testParameter = ["side"=>10,"perimeter"=>80,"area"=>482.842712];
+    public function setUp() : void {
+        $this->testParameter = ["side"=>10,"perimeter"=>80,"area"=>482.842712];
+        $this->Octagon = new Octagon($this->testParameter["side"]);
+    }
+
+    public function tearDown() : void {
+        unset ($this->Octagon);
+        unset($this->testParameter);
+    }
 
     public function testInitiateClass()
     {
-        $octagonClass = new Octagon($this->testParameter["side"]);
-
         $this->assertTrue(true);
-        $this->assertInstanceOf("Octagon", $octagonClass);
+        $this->assertInstanceOf("Octagon", $this->Octagon);
     }
 
     public function testCalculatePerimeterWithSideLength10() {
-        $octagonCalculator = new Octagon($this->testParameter["side"]);
-        $perimeter = $octagonCalculator->getPerimeter();
+        $perimeter = $this->Octagon->getPerimeter();
 
         $this->assertEquals($this->testParameter["perimeter"], $perimeter);
     }
 
     public function testCalculateAreaOfAnOctagonWithSideLength10() {
-        $octagonAreaCalculator = new Octagon($this->testParameter["side"]);
-        $area = $octagonAreaCalculator->getArea();
+        $area = $this->Octagon->getArea();
 
         $this->assertEquals($this->testParameter["area"], $area);
 
