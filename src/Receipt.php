a3f958148427a37907892e01ae440f80cbe870a1
Shavez <shavez00@Yahoo.com>
2023-01-19 23:51:21 +0000

    updated receipt test and created xml testsuite

--- src/Receipt.php
+++ src/Receipt.php
@@ -5,4 +5,15 @@ class Receipt {
     public function total(array $items = []){
         return array_sum($items);
     }
+
+    public function tax(float $inputAmount, float $taxInput) {
+        $output = NULL;
+        try {
+            $inputAmount = number_format($inputAmount, 2);
+            $output = round(($inputAmount * $taxInput), 2, PHP_ROUND_HALF_UP);
+            return $output;
+        } catch (exception $e) {
+            echo $e->getMessage();
+        }
+    }
 }
--- test/ReceiptTest.php
+++ test/ReceiptTest.php
@@ -24,4 +24,15 @@ class ReceiptTest extends TestCase {
             'When summing the total should equal 15'
         );
     }
+
+    public function testTax() {
+        $inputAmount = 10.00;
+        $taxInput = 0.10;
+        $output = $this->Receipt->tax($inputAmount, $taxInput);
+        $this->assertEquals(
+            1,
+            $output,
+            'The tax calculation should equal 1.00'
+        );
+    }
 }
