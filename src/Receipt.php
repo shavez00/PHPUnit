93538976474df3e42819d46243a7661417485b81
Shavez <shavez00@Yahoo.com>
2023-01-22 02:12:40 +0000

    updated receipt class to remove number_format

--- src/Receipt.php
+++ src/Receipt.php
@@ -18,7 +18,6 @@ class Receipt {
     public function tax(float $inputAmount, float $taxInput) {
         $output = NULL;
         try {
-            $inputAmount = number_format($inputAmount, 2);
             $output = round(($inputAmount * $taxInput), 2, PHP_ROUND_HALF_UP);
             return $output;
         } catch (exception $e) {
